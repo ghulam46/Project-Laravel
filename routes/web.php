ca5bd934b02d187a48d695dd9aeabd3208520381
ghulam46 <ghulam.yanuar01@gmail.com>
2023-03-09 19:53:36 +0700

    route & view blade

--- routes/web.php
+++ routes/web.php
@@ -13,6 +13,16 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
+// Route::get('/', function () {
+//     return view('welcome');
+// });
+
 Route::get('/', function () {
-    return view('welcome');
+    return view('home');
+});
+Route::get('/portfolio', function () {
+    return view('portfolio');
+});
+Route::get('/blog', function () {
+    return view('blog');
 });
